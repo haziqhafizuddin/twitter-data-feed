5a79aa025794662b1a74c319c6eb5f84b7ab219d
haziq <haziqhafizuddintamsir@gmail.com>
2016-01-27 12:08:59 +0800

    fixed grab data feed to csv

--- twitter4.py
+++ twitter4.py
@@ -1,5 +1,6 @@
 import encoding_fix
 import tweepy
+import csv
 from twitter_authentication import CONSUMER_KEY, CONSUMER_SECRET, ACCESS_TOKEN, ACCESS_TOKEN_SECRET
 
 auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
@@ -8,10 +9,11 @@ auth.set_access_token(ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
 api = tweepy.API(auth)
 
 # code to write the file
-output_file = open("MY_DATA.tsv", "w", encoding="utf-8")
+output_file = csv.writer(open("food_data.csv", "w"))
 
-public_tweets = api.search("data science", count=10)
+public_tweets = api.search("food", count=50)
 
 for tweet in public_tweets:
-    print(tweet.user.screen_name + "\t" + str(tweet.created_at) + "\t" + tweet.text, file=output_file)
+    print(tweet.user.screen_name + "\t" + str(tweet.created_at) + "\t" + tweet.text)
+    output_file.writerow([tweet.user.screen_name.encode("utf-8"), str(tweet.created_at), tweet.text.encode("utf-8")])
 
